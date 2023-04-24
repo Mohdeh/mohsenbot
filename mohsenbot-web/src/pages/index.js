7e112199630750c0d7068f3f94c8941ad58bca6a
Mohsen Dehhaghi <dehhaghi@usc.edu>
2023-04-24 02:35:53 +0000

    Added async message streaming

--- mohsenbot-web/src/pages/index.js
+++ mohsenbot-web/src/pages/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import Head from "next/head";
+import { createParser } from "eventsource-parser";
 
 const SYSTEM_MESSAGE =
   "I am Mohsenbot, a versatile AI agent created by Mohsen using state-of-the-art ML models and APIs!";
@@ -17,36 +18,81 @@ export default function Home() {
 
   const API_URL = "https://api.openai.com/v1/chat/completions";
 
-  async function sendRequest() {
-    // update message history
-    const newMessage = { role: "user", content: userMessage };
-    const newMessages = [...messages, newMessage];
+  // message streaming 
+  const sendRequest = async () => {
+    const updatedMessages = [
+      ...messages,
+      {
+        role: "user",
+        content: userMessage,
+      },
+    ];
 
-    setMessages(newMessages);
+    setMessages(updatedMessages);
     setUserMessage("");
 
-    const response = await fetch(API_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + apiKey,
-      },
-      body: JSON.stringify({
-        model: "gpt-3.5-turbo",
-        messages: newMessages,
-      }),
-    });
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: updatedMessages,
+          stream: true,
+        }),
+      });
 
-    const responseJson = await response.json();
-    const newBotMessage = responseJson.choices[0].message;
-    const newMessages2 = [...newMessages, newBotMessage];
-    setMessages(newMessages2);
+      const reader = response.body.getReader();
+
+      let newMessage = "";
+      const parser = createParser((event) => {
+        if (event.type === "event") {
+          const data = event.data;
+          if (data === "[DONE]") {
+            return;
+          }
+          const json = JSON.parse(event.data);
+          const content = json.choices[0].delta.content;
+
+          if (!content) {
+            return;
+          }
+
+          newMessage += content;
+
+          const updatedMessages2 = [
+            ...updatedMessages,
+            { role: "assistant", content: newMessage },
+          ];
+
+          setMessages(updatedMessages2);
+        } else {
+          return "";
+        }
+      });
+
+      // ** eslint-disable-next-line **
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        const text = new TextDecoder().decode(value);
+        parser.feed(text);
+      }
+    } catch (error) {
+      console.error("error");
+      window.alert("Error:" + error.message);
+    }
+  };
+
     // console.log("responseJson: ", responseJson);
 
     // // console.log("responsejson: ", responseJson);
     // setBotMessage(responseJson.choices[0].message.content);
     // // console.log("botMessage: ", botMessage);
-  }
+  
 
   return (
     <>
