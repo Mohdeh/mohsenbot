6b628fe7e9a434156429e1a95742f8e3ed44cc0f
Mohsen Dehhaghi <dehhaghi@usc.edu>
2023-04-24 02:41:24 +0000

    eslint-disable-next-line

--- mohsenbot-web/src/pages/index.js
+++ mohsenbot-web/src/pages/index.js
@@ -74,7 +74,7 @@ export default function Home() {
         }
       });
 
-      // ** eslint-disable-next-line **
+      // eslint-disable-next-line
       while (true) {
         const { done, value } = await reader.read();
         if (done) break;
